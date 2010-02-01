63be6e1c4c4b21371c8b450e57c922d719dd9813
Philemon Brakel <philemon.brakel@elis.ugent.be>
2010-02-01 11:06:27 +0100

    Added a leaky reservoir node.

--- src/Engine/reservoir_nodes.py
+++ src/Engine/reservoir_nodes.py
@@ -67,3 +67,47 @@ class ReservoirNode(mdp.Node):
             self.states[n] = self.state_nonlin
         
         return self.states
+
+
+class LeakyReservoirNode(ReservoirNode):
+
+    def __init__(self, input_dim=None, output_dim=None, spec_radius=0.9, 
+                 nonlin_func = 'tanh', bias = 0.0, input_scaling=1.0, leak_rate=1.0, dtype='float64'):
+        """ Initializes and constructs a random reservoir with leaky-integrator neurons.
+                
+        output_dim -- the number of outputs, which is also the number of
+                          neurons in the reservoir
+        prototype -- a prototype reservoir which will be cloned with all
+                     its parameters
+        """
+        super(LeakyReservoirNode, self).__init__(input_dim, output_dim, spec_radius,
+                                            nonlin_func, bias, input_scaling, dtype)
+        
+        self.leak_rate = leak_rate
+
+    def _execute(self, x):
+        """ Executes simulation with input vector x.
+        """
+        
+        nonlinearity = getattr(mdp.numx, self.nonlin_func)
+        steps = x.shape[0]
+        self.states = numpy.zeros((steps, self._output_dim), dtype=self._dtype)
+        self.state = numpy.zeros(self._output_dim)
+        self.state_nonlin = numpy.zeros(self._output_dim)
+        leak_rate = self.leak_rate
+
+        self.state += numpy.dot(self.Win, x[0])
+        self.state += self.Biasin
+        self.state_nonlin = nonlinearity(self.state)
+        self.states[0] = leak_rate * self.state_nonlin
+
+        for n in range(1, steps):
+            self.state = numpy.dot(self.W, self.state)
+            self.state += numpy.dot(self.Win, x[n])
+            self.state += self.Biasin
+            self.state_nonlin = nonlinearity(self.state)
+            
+            self.states[n] = (1 - leak_rate) * self.states[n - 1] + leak_rate * self.state_nonlin
+        
+        return self.states
+
