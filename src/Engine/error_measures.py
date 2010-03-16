b55cea3cd10b4e406eedf60782a155252fac2ac2
David Verstraeten <david.verstraeten@elis.ugent.be>
2010-03-16 10:23:58 +0100

    Added unit tests for ReservoirNode, converted analog_speech example to use flows instead of flownodes.

--- src/Engine/error_measures.py
+++ src/Engine/error_measures.py
@@ -1,15 +1,7 @@
-from __future__ import division
 import numpy as np
 import mdp
 
-__all__ = [
-    'nrmse',
-    'nmse',
-    'rmse',
-    'mse'
-    ]
-
-def nrmse( input, target, discard=0, var=-1 ):
+def nrmse(input, target):
     """ NRMSE calculation.
     
     Calculates the normalized root mean square error (NRMSE)
@@ -21,32 +13,24 @@ def nrmse( input, target, discard=0, var=-1 ):
             the input signal
         target : array
             the target signal
-        discard : int, optional
-            number of initial values which should be skipped
-        var : float, optional
-            can be used to set the variance of the target signal
     """
-    insignal = input.copy()
+    input = input.copy()
     targetsignal = target.copy()
     
     # reshape values
-    insignal.shape = -1,
+    input.shape = -1,
     targetsignal.shape = -1,
     
-    if( targetsignal.size != insignal.size ):
+    if( targetsignal.size != input.size ):
         raise RuntimeError('Length of target signal is not equal to length of generated signal.')
     
-    # check if a variance is given
-    if var<=0:
-        # Use normalization with N-1, as in matlab
-        var = targetsignal.std(ddof=1)**2
+    # Use normalization with N-1, as in matlab
+    var = targetsignal.std(ddof=1)**2
     
-    error = (targetsignal - insignal)**2
-    nrmse = np.sqrt( error.mean() / var )
+    error = (targetsignal - input)**2
+    return np.sqrt( error.mean() / var )
     
-    return nrmse
-
-def nmse( input, target, discard=0, var=-1 ):
+def nmse( input, target):
     """ NMSE calculation.
     
     Calculates the normalized mean square error (NMSE)
@@ -54,40 +38,27 @@ def nmse( input, target, discard=0, var=-1 ):
     
     Parameters:
     -----------
-        insignal : array
+        input : array
             the input signal
         targetsignal : array
             the target signal
-        discard : int, optional
-            number of initial values which should be skipped
-        var : float, optional
-            can be used to set the variance of the target signal
     """
-    insignal = input.copy()
+    input = input.copy()
     targetsignal = target.copy()
     
     # reshape values
-    insignal.shape = -1,
+    input.shape = -1,
     targetsignal.shape = -1,
     
-    if( targetsignal.size > insignal.size ):
-        maxsize = insignal.size
-    else:
-        maxsize = targetsignal.size
+    if( targetsignal.size != input.size ):
+        raise RuntimeError('Length of target signal is not equal to length of generated signal.')
     
-    origsig = targetsignal[discard:maxsize]
-    testsig = insignal[discard:maxsize]
+    var = targetsignal.std()**2
     
-    # check if a variance is given
-    if var<=0:
-        var = origsig.std()**2
+    error = (targetsignal - input)**2
+    return error.mean() / var
     
-    error = (origsig - testsig)**2
-    nmse = error.mean() / var
-    
-    return nmse
-
-def rmse( input, target, discard=0 ):
+def rmse( input, target):
     """ RMSE calculation.
     
     Calculates the root mean square error (RMSE)
@@ -95,34 +66,22 @@ def rmse( input, target, discard=0 ):
     
     Parameters:
     -----------
-        insignal : array
+        input : array
             the input signal
         targetsignal : array
             the target signal
-        discard : int, optional
-            number of initial values which should be skipped
     """
-    insignal = input.copy()
+    input = input.copy()
     targetsignal = target.copy()
     
     # reshape values
-    insignal.shape = -1,
+    input.shape = -1,
     targetsignal.shape = -1,
+            
+    error = (targetsignal - input)**2
+    return np.sqrt( error.mean() )
     
-    if( targetsignal.size > insignal.size ):
-        maxsize = insignal.size
-    else:
-        maxsize = targetsignal.size
-    
-    origsig = targetsignal[discard:maxsize]
-    testsig = insignal[discard:maxsize]
-    
-    error = (origsig - testsig)**2
-    nrmse = np.sqrt( error.mean() )
-    
-    return nrmse
-
-def mse( input, target, discard=0 ):
+def mse(input, target):
     """ MSE calculation.
     
     Calculates the mean square error (MSE)
@@ -130,41 +89,26 @@ def mse( input, target, discard=0 ):
     
     Parameters:
     -----------
-        insignal : array
+        input : array
             the input signal
         targetsignal : array
             the target signal
-        discard : int, optional
-            number of initial values which should be skipped
     """
-    insignal = input.copy()
+    
+    input = input.copy()
     targetsignal = target.copy()
     
     # reshape values
-    insignal.shape = -1,
+    input.shape = -1,
     targetsignal.shape = -1,
-    
-    if( targetsignal.size > insignal.size ):
-        maxsize = insignal.size
-    else:
-        maxsize = targetsignal.size
-    
-    origsig = targetsignal[discard:maxsize]
-    testsig = insignal[discard:maxsize]
-    
-    error = (origsig - testsig)**2
-    nrmse = error.mean()
-    
-    return nrmse
+        
+    error = (targetsignal - input)**2
+    return error.mean()
 
-def zero_one_loss(input, target, discard=0):
-    """ Compute mean zero-one loss function
+def loss_01(input, target):
+    """ Compute zero one loss function 
     
-    This error measure is the zero-one loss:
-    - if the input signal is not equal to the desired signal, one is added to the error
-    - if the input signal is equal to the desired signal, nothing is added.
-    This error is then averaged over all timesteps
-    
+    Returns the fraction of timesteps where input is unequal to target
     
     Parameters:
     -----------
@@ -172,59 +116,16 @@ def zero_one_loss(input, target, discard=0):
             the input signal
         target : array
             the target signal
-        discard : int, optional
-            number of initial values which should be skipped
-    """
-    insignal = input.copy()
-    targetsignal = target.copy()
-    
-    # reshape values
-    insignal.shape = -1,
-    targetsignal.shape = -1,
-    
-    if( targetsignal.size > insignal.size ):
-        maxsize = insignal.size
-    else:
-        maxsize = targetsignal.size
-    
-    targetsignal = targetsignal[discard:maxsize]
-    insignal = insignal[discard:maxsize]
-    
-    print insignal!=targetsignal
-    
-    error = np.sum(insignal!=targetsignal)
-    print error
-    zero_one_loss = error.mean()
-    
-    return zero_one_loss
-
-def loss_01(input, target, discard=0):
-    """ Compute word error rate
-    
-    This error measure is the word error rate:
-    - class labels are assumed to be -1 and +1
-    - If all class labels are equal, error is zero
-    - If some class labels are not equal, error is one.
-    Input and target are assumed to be vectors (single timestep).
-    
-    Parameters:
-    -----------
-        input : array
-            the input signal
-        target : array
-            the target signal
-        discard : int, optional
-            number of initial values which should be skipped
     """
     if input.shape!=target.shape:
         raise RuntimeError("Input and target should have the same shape")
-    #print input, target
-    return sum(input[:] !=target[:])    
 
-def cosine(x, y):
-    ''' Compute cosine of angle between two vector
+    return (input[:] !=target[:]).mean()
+
+def cosine(input, target):
+    ''' Compute cosine of the angle between two vectors
     
     This error measure measures the extent to which two vectors point in the same direction. 
     A value of 1 means complete alignment, a value of 0 means the vectors are orthogonal.
     '''
-    return mdp.numx.dot(x, y) / (mdp.numx.linalg.norm(x) * mdp.numx.linalg.norm(y))
+    return float(mdp.numx.dot(input, target)) / (mdp.numx.linalg.norm(input) * mdp.numx.linalg.norm(target))
--- src/Engine/tests/reservoir_nodes_test.py
+++ src/Engine/tests/reservoir_nodes_test.py
@@ -7,8 +7,44 @@ Created on Dec 7, 2009
 import sys
 import mdp
 import nose
-import Engine.reservoir_nodes
+import unittest
+import reservoir_nodes
+import utility_functions
+import scipy as sp
 
-def reservoir_test():
-    r = Engine.reservoir_nodes.ReservoirNode (output_dim = 10)
-    #assert_almost_equals(E.utility_functions.get_spectral_radius(r.W), sp.max(sp.abs(sp.linalg.eigvals(r.W)))) 
+class ReservoirNodeTest(unittest.TestCase):
+    def setUp(self):
+        self.default_size = 10
+        self.input_length = 10
+
+    def test_spectral_radius(self):
+        ''' Test if spectral radius is expected value '''
+
+        # Default value
+        r = reservoir_nodes.ReservoirNode (output_dim = self.default_size)
+        nose.tools.assert_almost_equals(utility_functions.get_spectral_radius(r.w), sp.amax(sp.absolute(sp.linalg.eigvals(r.w)))) 
+
+        # Custom value
+        rho = 0.1
+        r = reservoir_nodes.ReservoirNode (output_dim = self.default_size, spectral_radius=rho)
+        nose.tools.assert_almost_equals(utility_functions.get_spectral_radius(r.w), rho) 
+
+    def test_zero_input(self):
+        ''' Test if zero input returns zero output without bias
+        '''
+        r = reservoir_nodes.ReservoirNode (output_dim = self.default_size)
+        assert sp.all(r(sp.zeros((self.input_length,1))) == sp.zeros((self.input_length,self.default_size)))
+
+    def test_input_mapping(self):
+        ''' Test if input mapping is correct for zero internal weight matrix 
+        '''
+        r = reservoir_nodes.ReservoirNode (output_dim = self.default_size, spectral_radius=0)
+        assert sp.all(r(sp.ones((self.input_length,1))) == sp.tanh(sp.dot(sp.ones((self.input_length,1)), r.w_in.T)))
+        
+    def test_bias(self):
+        ''' Test if turning on bias gives expected results 
+        '''
+        r = reservoir_nodes.ReservoirNode (output_dim = self.default_size, spectral_radius=0, bias_scaling = 1)
+        assert sp.all(r(sp.zeros((self.input_length,1))) == sp.tile(sp.tanh(r.w_bias), (self.input_length, 1)))
+
+
