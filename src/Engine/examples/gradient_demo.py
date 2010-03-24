2886bd670811b23e7171482e83e0e50a3157cf20
bschrauw <devnull@localhost>
2010-03-24 20:13:02 +0100

    - updated gradient_demo

--- src/Engine/examples/gradient_demo.py
+++ src/Engine/examples/gradient_demo.py
@@ -1,49 +1,43 @@
 # Shows the use of the gradient based learning module.
-import mdp
 import pylab
-import numpy as np
-from mdp.utils import mult
 from Engine.nonlinear_nodes import PerceptronNode
-from Engine.gradient.gradient_nodes import BackpropNode, GradientPerceptronNode
+from Engine.gradient.gradient_nodes import BackpropNode 
 from Engine.gradient.trainers import *
 from Engine.utility_functions import *
 from Engine.error_measures import mse
 
 # Generate training data: a random sample from the sine function
 data_in = np.random.uniform(-1, 1, 50)
-data_in[0] = -1;
-data_in[1] = 1;
-data_in.shape = (50, 1)
+#data_in = np.arange(-1, 1, .11)
+data_in.shape += (1,)
 data_out = np.sin(data_in * 5 * np.pi)
 
-# Generate 1-3-1 MLP 
-percnode1 = GradientPerceptronNode(1, 12, transfer_func=TanhFunction)
-percnode2 = GradientPerceptronNode(12, 1)
+# Generate 1-12-1 MLP 
+percnode1 = PerceptronNode(1, 12, transfer_func=TanhFunction)
+percnode2 = PerceptronNode(12, 1)
 myflow = percnode1 + percnode2
 
-#bpnode = BackpropNode(myflow, gradient_descent)
-#bpnode.train(x=data_in, t=data_out, epochs=1500, learning_rate=.001, momentum=.3)
-
-choice = input('What optimization method do you want to use?\n0: Conjugate gradient\n1: BFGS (low memory Newton)\n2: gradient descent\n3: RPROP\n...')
-choices = [CGTrainer(), BFGSTrainer(), GradientDescentTrainer(epochs=30000), RPROPTrainer(epochs=30000)]
+choice = input("""What optimization method do you want to use?
+0: Conjugate gradient
+1: BFGS (low memory Newton)
+2: gradient descent
+3: RPROP
+4: L-BFGS-B
+...""")
+choices = [CGTrainer(), BFGSTrainer(), GradientDescentTrainer(epochs=30000), RPROPTrainer(epochs=30000),LBFGSBTrainer(weight_bounds=(-10,10))]
 
 bpnode = BackpropNode(myflow, choices[choice], loss_func=mse)
 bpnode.train(x=data_in, t=data_out)
 
-data_in2 = np.random.uniform(-1, 1, 500)
-data_in2.shape = (500, 1)
+data_in2 = np.arange(-2, 2, .005)
+data_in2.shape += (1,)
 data_out2 = np.sin(data_in2 * 5 * np.pi)
 
 out = bpnode(data_in2)
 
-pylab.subplot(311)
+print "Done!"
+
 pylab.scatter(data_in, np.array(data_out))
-pylab.title('Train data')
-pylab.subplot(312)
-pylab.scatter(data_in2, out)
-pylab.title('Network output')
-pylab.subplot(313)
-pylab.scatter(data_in2, data_out2)
-pylab.title('True function')
+pylab.plot(data_in2, out, data_in2, data_out2)
 pylab.show()
 
