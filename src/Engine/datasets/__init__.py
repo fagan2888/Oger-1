075d404304bc22eda754d4b4c04d640b0344003d
Philemon Brakel <philemon.brakel@elis.ugent.be>
2010-03-30 13:45:23 +0200

    Removed dependency on elman grammar.

--- src/Engine/datasets/__init__.py
+++ src/Engine/datasets/__init__.py
@@ -1,5 +1,5 @@
 from datasets import (narma30, mackey_glass, analog_speech, timit_tiny, mso)
-from grammars import (elman_grammar, simple_pcfg)
+from grammars import (simple_pcfg)
 
 # clean up namespace
 del datasets
