67f5a2ec0bfd23a892c2762097d94b32ea9ca045
bschrauw <devnull@localhost>
2010-03-31 09:26:14 +0200

    - add lorentz dataset - add memory capacity dataset - add memory capacity error measure - reorganize dataset files

--- src/Engine/datasets/__init__.py
+++ src/Engine/datasets/__init__.py
@@ -3,12 +3,16 @@ This subpackage contains several standard datasets.
 """
 
 
-from datasets import (narma30, mackey_glass, analog_speech, timit_tiny, mso)
-from grammars import (simple_pcfg)
+from datasets import (narma30,memtest,)
+from speech_datasets import (analog_speech, timit_tiny,)
+from timeseries_datasets import (mackey_glass, mso, lorentz,)
+from grammars import (simple_pcfg,)
 
 # clean up namespace
 del datasets
+del speech_datasets
+del timeseries_datasets
 del grammars
 
-__all__=['narma30','mackey_glass','analog_speech','timit_tiny',' mso','simple_pcfg']
+__all__=['narma30','mackey_glass','analog_speech','timit_tiny',' mso','simple_pcfg','lorentz','memtest']
 
