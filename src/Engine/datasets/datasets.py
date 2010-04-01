60c14730598aee1745f2ba7612e8f0686d0bffee
bschrauw <devnull@localhost>
2010-04-01 14:21:53 +0200

    - add narma10

--- src/Engine/datasets/datasets.py
+++ src/Engine/datasets/datasets.py
@@ -5,6 +5,30 @@ Created on Aug 20, 2009
 '''
 import mdp
 
+def narma10(n_samples=10, sample_len=1000):
+    ''' 
+    narma10(n_samples=10, sample_len=1000) -> inputs, outputs
+    Return data for the 10th order NARMA task. 
+    Parameters are:
+        - sample_len: length of the time-series in timesteps, default 1000
+        - n_samples: number of example timeseries to be generated, default 10
+        
+    WARNING: this is an unstable dataset. There is a small chance the system becomes 
+    unstable, leading to an unusable dataset. It is better to use NARMA30 which 
+    where this problem happens less often.
+    '''
+    system_order = 10
+    inputs, outputs = [], []
+    for sample in range(n_samples):
+        inputs.append(mdp.numx.random.rand(sample_len, 1) * .5)
+        inputs[sample].shape = (-1, 1)
+        outputs.append(mdp.numx.zeros((sample_len, 1)))
+        for k in range(system_order, sample_len - 1):
+            outputs[sample][k + 1] = .3 * outputs[sample][k] + .05 * \
+            outputs[sample][k] * mdp.numx.sum(outputs[sample][k - 9:k]) + \
+            1.5 * inputs[sample][k - 9] * inputs[sample][k] + .1
+    return inputs, outputs 
+
 def narma30(n_samples=10, sample_len=1000):
     ''' 
     narma30(n_samples=10, sample_len=1000) -> inputs, outputs
