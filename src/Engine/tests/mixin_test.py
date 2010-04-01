98b42f8e0d8dc67de3ad877aa0984f1d8a01a44b
bschrauw <devnull@localhost>
2010-04-01 18:39:45 +0200

    - final mixin fix

--- src/Engine/tests/mixin_test.py
+++ src/Engine/tests/mixin_test.py
@@ -8,12 +8,9 @@ class MixinTest(unittest.TestCase):
 
     def test_washout_mixin_unsup(self):
         x = mdp.numx.zeros((100,1000))        
+        Engine.utils.enable_washout(mdp.nodes.PCANode)
         pca = mdp.nodes.PCANode()
-        Engine.utils.enable_washout(pca)
         pca.washout = 10
-
-        assert(hasattr(pca, "washout"))
-        assert(not hasattr(mdp.nodes.PCANode, "washout"))
         
         assert(pca.is_trainable())
         y = pca(x)
@@ -23,8 +20,8 @@ class MixinTest(unittest.TestCase):
     def test_washout_mixin_sup(self):
         x = mdp.numx.zeros((100,1000))        
         y = mdp.numx.zeros((100,1000))        
+        Engine.utils.enable_washout(mdp.nodes.LinearRegressionNode, 10)
         lin = mdp.nodes.LinearRegressionNode()
-        Engine.utils.enable_washout(lin, 10)
         
         assert(lin.is_trainable())
         lin.train(x, y=y)
--- src/Engine/utils/mixin.py
+++ src/Engine/utils/mixin.py
@@ -1,32 +1,37 @@
-    
-def mix_in(OriginalClass, mixInClass):
+  
+def mix_in(original, mixin):
     """
     This helper function allows to dynamically add a new base class to the given class.
     It is injected at the top of the base class hierarchy.     
     """
-    if mixInClass not in OriginalClass.__bases__:
-        OriginalClass.__bases__ = (mixInClass,) + OriginalClass.__bases__
+    if mixin not in original.__bases__:
+        original.__bases__ = (mixin,) + original.__bases__
 
 def enable_washout(original, washout=0):
     """
-    This helper function injects additional code in the given object such
+    This helper function injects additional code in the given class such
     that during training the first N timesteps are disregarded. This can be applied
     to all trainable nodes, both supervised and unsupervised.
     """
     
-    if isinstance(original, type):
-        raise Exception('Washout can only be enabled on objects, not classes.')        
+    if not isinstance(original, type):
+        raise Exception('Washout can only be enabled on classes.')        
 
     if not hasattr(original, "_train"):
         raise Exception('Object should have a _train method.')
 
-    def _train(x, *args, **kwargs):
-        if len(args) > 0:
-            original._train_no_washout(x[original.washout:,:], args[0][original.washout:,:], **kwargs)
-        else:
-            original._train_no_washout(x[original.washout:,:], **kwargs)
-    
+    # helper washout class
+    class Washout:
+        def _train(self, x, *args, **kwargs):
+            if len(args) > 0:
+                self._train_no_washout(x[self.washout:,:], args[0][self.washout:,:], **kwargs)
+            else:
+                self._train_no_washout(x[self.washout:,:], **kwargs)
+                
+    # inject new methods
     setattr(original, "_train_no_washout", original._train)    
-    setattr(original, "_train", _train)
+    setattr(original, "_train", Washout._train)
     setattr(original, "washout", washout)
 
+    # add to base classes
+    original.__bases__ += (Washout,)
