d9883f10c57e8ff102e0ff1df1b265bd034d6b37
David Verstraeten <david.verstraeten@elis.ugent.be>
2010-10-01 15:32:13 +0200

    Added demo file for CUDA TRBM

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,16 @@
 from distutils.core import setup
 
-short_description = "Engine Toolbox"
+short_description = "Oger Toolbox"
 
 long_description = """
-The Engine is a Python toolbox for rapidly building, training and evaluating hierarchical learning architectures on large datasets. It builds functionality on top of the Modular toolkit for Data Processing (MDP).  The Engine builds functionality on top of MDP, such as:
+Oger is a Python toolbox for rapidly building, training and evaluating hierarchical learning architectures on large datasets. It builds functionality on top of the Modular toolkit for Data Processing (MDP). Oger builds functionality on top of MDP, such as:
  - Cross-validation of datasets
  - Grid-searching large parameter spaces
  - Processing of temporal datasets
  - Gradient-based training of deep learning architectures
  - Interface to the Speech Processing, Recognition, and Automatic Annotation Kit (SPRAAK) 
 
- In addition, several additional MDP nodes are provided by the Engine, such as a:
+ In addition, several additional MDP nodes are provided by the Oger, such as a:
  - Reservoir node
  - Leaky reservoir node
  - Ridge regression node
@@ -25,18 +25,18 @@ classifiers = ["Intended Audience :: Developers",
                "Topic :: Scientific/Engineering :: Mathematics"]
 
 
-setup(name = 'Engine', version = '0.2',
+setup(name = 'Oger', version = '1.0',
       author = "Philemon Brakel, Martin Fiers, Fiontann O'Donnell, Benjamin Schrauwen and David Verstraeten",
       author_email = 'pbpop3@gmail.com, mfiers@intec.ugent.be, fodonnel@elis.ugent.be, benjamin.schrauwen@elis.ugent.be, david.verstraeten@elis.ugent.be',
       maintainer = "Philemon Brakel, Martin Fiers, Fiontann O'Donnell, Benjamin Schrauwen and David Verstraeten",
       maintainer_email = 'pbpop3@gmail.com, mfiers@intec.ugent.be, fodonnel@elis.ugent.be, benjamin.schrauwen@elis.ugent.be, david.verstraeten@elis.ugent.be',
       platforms = ["Any"],
       url = 'http://organic.elis.ugent.be',
-      download_url = 'http://organic.elis.ugent.be/Engine',
+      download_url = 'http://organic.elis.ugent.be/organic/oger',
       description = short_description,
       long_description = long_description,
       classifiers = classifiers,
-      packages = ['Engine', 'Engine.datasets', 'Engine.evaluation', 'Engine.gradient', 'Engine.utils', 'Engine.nodes', 'Engine.parallel',
+      packages = ['Oger', 'Oger.datasets', 'Oger.evaluation', 'Oger.gradient', 'Oger.utils', 'Oger.nodes', 'Oger.parallel',
                   'mdp', 'mdp.nodes', 'mdp.utils', 'mdp.hinet', 'mdp.test', 'mdp.demo', 'mdp.graph', 'mdp.contrib', 'mdp.parallel'],
-      package_data={'mdp': ['hinet/*.css', 'utils/*.css'], 'Engine': ['examples/*.py']},
+      package_data={'mdp': ['hinet/*.css', 'utils/*.css'], 'Oger': ['examples/*.py']},
       )
