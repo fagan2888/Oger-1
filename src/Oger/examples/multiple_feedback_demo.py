d9cc4288e569d322cd5c9704fa9bbe507fe731f0
David Verstraeten <david.verstraeten@elis.ugent.be>
2011-05-16 09:25:23 +0200

    Rewrite multiple feedback demo with FreerunFlow.

--- src/Oger/examples/multiple_feedback_demo.py
+++ src/Oger/examples/multiple_feedback_demo.py
@@ -18,61 +18,45 @@ if __name__ == "__main__":
         This example is perhaps not very practical, but it shows how more complex feedback schemes can be built.
     '''
     freerun_steps = 500
-    
+
     N = 100;
-    x = Oger.datasets.mackey_glass(sample_len=10000)
-    xtrain = x[0][0:-freerun_steps]
-    ytrain = xtrain[1:]
-    
-    #The reservoir expects two-dimensional input, so we just duplicate the teacher signal
-    xtrain = mdp.numx.hstack((xtrain[0:-1], xtrain[0:-1]))
-        
+    x = Oger.datasets.mackey_glass(sample_len=1000)
+
     # construct individual nodes
-    reservoir = Oger.nodes.FeedbackReservoirNode(output_dim=N, input_dim=2, input_scaling=1)    
-    Oger.utils.mix_in(Oger.nodes.FeedbackReservoirNode, Oger.nodes.LeakyReservoirNode)
-    reservoir.leak_rate = 0.5    
-    
-    readout = Oger.nodes.RidgeRegressionNode(0.0001, input_dim=N)
-    readout2 = Oger.nodes.RidgeRegressionNode(0.0002, input_dim=N) 
-    
+    reservoir = Oger.nodes.LeakyReservoirNode(output_dim=N, input_dim=1, input_scaling=1, reset_states=False)
+    reservoir.leak_rate = 0.5
+
+    readout = Oger.nodes.RidgeRegressionNode(.0001, input_dim=N, output_dim=1)
+    readout2 = Oger.nodes.RidgeRegressionNode(0.00001, input_dim=N, output_dim=1)
+
     output_layers = mdp.hinet.SameInputLayer([readout, readout2])
-    fb = Oger.nodes.FeedbackNode(n_timesteps=freerun_steps, input_dim=2)
 
+    readout3 = Oger.nodes.RidgeRegressionNode(0.01, input_dim=2)
 
-    readout3 = Oger.nodes.RidgeRegressionNode(0.000001, input_dim=2)
-    # build network with MDP framework
-    flow = Oger.nodes.InspectableFlow([reservoir, output_layers, fb, readout3], verbose=1)
-        
+    flow = Oger.nodes.FreerunFlow([reservoir, output_layers, readout3], freerun_steps=freerun_steps, verbose=1)
+
+    flow._states = [[] for _ in range(len(flow.flow))]
+    Oger.utils.mix_in(Oger.nodes.FreerunFlow, Oger.nodes.InspectableFlow)
     # Train the reservoir to do one-step ahead prediction using the teacher-forced signal
-    flow.train([ [] , [[xtrain, ytrain]], [[xtrain]], [[xtrain, ytrain]]])
-                    
-    # Save the states of the reservoir during training for later plotting
-    y = flow.execute(xtrain)
-    training_states = flow.inspect(reservoir)
-    training_output = flow.inspect(output_layers)
-     
-    pylab.figure()
-    pylab.plot(y)
-    reservoir.reset_states = False
-    
-    # Without this line, we start from the estimated last timestep, while
-    # this line starts the feedback node from the actual last timestep.
-    fb.last_value = mdp.numx.atleast_2d(xtrain[-1, :]) 
+    flow.train([[], x, x])
 
-    # Execute the flow using the feedbacknode as input
-    output = flow.execute(fb)
-        
-    #collected_states=mdp.numx.array(reservoir.collected_states)
-    pylab.subplot(311)
-    #pylab.plot(mdp.numx.concatenate((mdp.numx.array(training_states[-freerun_steps:,:]), collected_states[0:freerun_steps,:])))
-    pylab.plot(training_states[-freerun_steps:, :])
-    
-    pylab.subplot(312)
-    pylab.plot(mdp.numx.concatenate((training_states[-freerun_steps:], flow.inspect(reservoir)[0:freerun_steps])))
-    
-    pylab.subplot(313)
-    pylab.plot(mdp.numx.concatenate((xtrain[-freerun_steps:, 1], output[0:freerun_steps, 0])))
-    pylab.plot(x[0][-2 * freerun_steps:])
-    pylab.plot(training_output[-freerun_steps:])
+    # build network with MDP framework
+    # Save the states of the reservoir during training for later plotting
+
+    z = flow.inspect(output_layers)
+    y = flow.execute(x[0][0])
+#    training_states = flow.inspect(reservoir)
+#    training_output = flow.inspect(output_layers)
+
+    pylab.figure()
+    #pylab.plot(y)
+    reservoir.reset_states = False
+
+
+    #pylab.subplot(313)
+    pylab.plot(x[0][0][-2 * freerun_steps:])
+    pylab.plot(y[-2 * freerun_steps:, :])
+    pylab.plot(z[0][-2 * freerun_steps:, 0])
+    pylab.plot(z[0][-2 * freerun_steps:, 1])
 
     pylab.show()
