22785adedc98251bfe3a1acbe1ff23cb7f76dc4d
Pieter Buteneers <devnull@localhost>
2011-09-30 12:01:53 +0200

    ELM_demo extended

--- src/Oger/examples/ELM_demo.py
+++ src/Oger/examples/ELM_demo.py
@@ -1,4 +1,5 @@
-# This is a demo for an OP-ELM that tries to model a sinc function
+# This is a demo for an ELM and OP-ELM that tries to model a sinc function
+# OP-ELM often performs better but not always BFS is slower but should perform even better...
 
 import mdp
 import Oger
@@ -7,29 +8,66 @@ import pylab
 
 X_orr = np.atleast_2d(np.arange(-5, 5, 0.025)).T
 Y_orr = np.sinc(X_orr)
+#shuffle data and add noise
 P = np.random.permutation(len(X_orr))
 X, Y = X_orr[P,:], Y_orr[P,:] + (np.random.rand(len(X_orr),1)-.5)/4
 
-n_folds = 20
+#split the data for training
+n_folds = 10
 l = np.floor(len(X)/n_folds)
 x,y=[],[]
 for i in range(n_folds-1):
     x.append(X[i*l:(i+1)*l,:])
     y.append(Y[i*l:(i+1)*l,:])
 
-#test data
+#last fold as test data
 x_t = X[(n_folds-1)*l:,:]
 y_t = Y_orr[P[(n_folds-1)*l:],:]
 
-
+#elm node simulates elm without training
 elm = Oger.nodes.ELMNode()
+
+#ELM training using Linear Regression (no regularization)
+readout = Oger.nodes.RidgeRegressionNode(ridge_param=0)
+flow = mdp.Flow([elm, readout],verbose=True)
+print '\nTraining ELM...\n'
+flow.train([x, zip(x,y)])
+yh_t_elm = flow(x_t) #ELM test output
+
+#OP-ELM training using Optimally Pruned Linear Regression (no regularization)
 readout = Oger.nodes.OPRidgeRegressionNode(ridge_param=0)
 flow = mdp.Flow([elm, readout],verbose=True)
+print '\nTraining OP-ELM...\n'
 flow.train([x, zip(x,y)])
-yh_test = flow(x_t)
-print '\nTest NRMSE =', Oger.utils.nrmse(yh_test, y_t), '\n'
+yh_t_opelm = flow(x_t) #OP-ELM test output
+
+#Backward Feature Selection training using BFSRidgeRegressionNode (with regularization)
+readout = Oger.nodes.BFSRidgeRegressionNode()
+flow = mdp.Flow([elm, readout],verbose=True)
+print '\nTraining Backward Feature Selection with regularization...\n'
+flow.train([x, zip(x,y)])
+yh_t_bfs = flow(x_t) #OP-ELM test output
+
+
+print '\nELM: Test NRMSE =', Oger.utils.nrmse(yh_t_elm, y_t)
+print 'OP-ELM: Test NRMSE =', Oger.utils.nrmse(yh_t_opelm, y_t)
+print 'BFS with regularization: Test NRMSE =', Oger.utils.nrmse(yh_t_bfs, y_t), '\n'
+
 
 pylab.scatter(X,Y, c='k', marker='+')
 pylab.plot(X_orr, Y_orr, linewidth=2)
-pylab.scatter(x_t,yh_test,c='r',linewidths=2)
+pylab.scatter(x_t,yh_t_elm,c='r',linewidths=2)
+pylab.title('ELM')
+
+pylab.figure()
+pylab.scatter(X,Y, c='k', marker='+')
+pylab.plot(X_orr, Y_orr, linewidth=2)
+pylab.scatter(x_t,yh_t_opelm,c='r',linewidths=2)
+pylab.title('OP-ELM')
+
+pylab.figure()
+pylab.scatter(X,Y, c='k', marker='+')
+pylab.plot(X_orr, Y_orr, linewidth=2)
+pylab.scatter(x_t,yh_t_opelm,c='r',linewidths=2)
+pylab.title('Backward Feature Selection with regularization')
 pylab.show()
